71f6457ab50d22cf4564f459813ac437576b821d
Aditya Adi <47720423+adi-001@users.noreply.github.com>
2020-03-17 23:36:07 +0530

    Update currency converter.py

--- currency converter.py
+++ currency converter.py
@@ -4,11 +4,16 @@ import requests, json
 
 api_key = "---Copy your api here---"
 
-base_url = "http://metals-api.com/api/"
-url = "http://metals-api.com/api/latest?access_key="+api_key
+# Asks for user to input base currency
+base_currency = str(input('\nWhat will be your base currency ?\nChoose from above currency code'))
+
+#Asks for user to input desired currency 
+desired_currency_exchange = str(input('Desired Currency : '))
+
+complete_url = "http://metals-api.com/api/convert?access_key="+api_key+"&from="+base_currency+"&to="+desired_currency_exchange+"&amount=1"
 
 # GET requests method to retrieve data from specified url
-response = requests.get(url)
+response  = requests.get(complete_url)
 
 # Getting the retrieved data in json format
 x = response.json()
@@ -21,15 +26,5 @@ for i in x:
 for _ in x['rates']:
     print(_,end='\t')   '''
 
-#Input 
-base_currency = str(input('\nWhat will be your base currency ?\nChoose from above currency code'))
-
-desired_currency_exchange = str(input('Desired Currency : '))
-
-complete_url = "http://metals-api.com/api/convert?access_key="+api_key+"&from="+base_currency+"&to="+desired_currency_exchange+"&amount=1"
-
-response  = requests.get(complete_url)
-
-x = response.json()
 
 print('hence 1'+base_currency+' = '+str(round(x['result'],2))+desired_currency_exchange)
